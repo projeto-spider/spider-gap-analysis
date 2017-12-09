6bdce25e15cc7b99cdfed7f5524ba0eb1064c560
lubien <lubien1996@gmail.com>
2017-12-09 10:22:40 -0300

    Setup calendar portuguese names on buefy

--- resources/plugins/buefy.js
+++ resources/plugins/buefy.js
@@ -1,4 +1,35 @@
 import Vue from 'vue'
 import Buefy from 'buefy'
 
-Vue.use(Buefy)
+Vue.use(Buefy, {
+  defaultIconPack: 'fa',
+
+  defaultDayNames: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+
+  defaultMonthNames: [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro'
+  ],
+
+  defaultDateFormatter(date){
+    const d = date.getDate()
+    const m = date.getMonth() + 1
+    const yyyy = date.getFullYear();
+
+    return twoDigits(d) + '/' + twoDigits(m) + '/' + yyyy
+  }
+})
+
+function twoDigits(n) {
+  return n < 10 ? `0${n}` : n
+}
