f4f3a2da9e12d5928d1d2eab1fad7e8da1e43e25
lubien <lubien1996@gmail.com>
2018-03-06 12:23:57 -0300

    Show organization name on Unit list

--- resources/pages/unit/index.vue
+++ resources/pages/unit/index.vue
@@ -8,16 +8,16 @@
       <thead>
         <tr>
           <th>ID</th>
+          <th>Organização</th>
           <th>Nome</th>
-          <th>Coordenador</th>
           <th>Ações</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="unit in units">
           <th>{{unit.id}}</th>
+          <td>{{organizations[unit.organization_id].name}}</td>
           <td>{{unit.name}}</td>
-          <td>{{unit.coordinator}}</td>
           <td>
             <nuxt-link :to="`/unit/${unit.id}`">
               <button class="button is-primary">Ver</button>
@@ -35,9 +35,15 @@ export default {
 
   async asyncData({ app }) {
     const units = await app.$axios.$get('/units')
+    const organizations = (await app.$axios.$get('/organizations'))
+      .reduce((acc, org) => {
+        acc[org.id] = org
+        return acc
+      }, {})
 
     return {
-      units
+      units,
+      organizations
     }
   }
 }
