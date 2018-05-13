25ab87219b6de794f165b40cf12499e17ff335e2
lubien <lubien1996@gmail.com>
2018-05-13 18:53:05 -0300

    Remove noise from Evaluation

--- resources/pages/evaluation/_id.vue
+++ resources/pages/evaluation/_id.vue
@@ -144,59 +144,36 @@
 import FormData from 'form-data'
 import expectedResults from '~/static/expected-results.json'
 import levels from '~/static/levels.json'
-import process from '~/static/process.json'
-const processes = process
-import processAttributes from '~/static/process-attributes.json'
-
-const emptyProjectEvidence = {
-  projectId: 0,
-  evidenceId: 0,
-  roleId: 0,
-  process: 0,
-  filename: 0,
-  type: 0,
-  typeId: 0,
-}
-
-const flatten = xs =>
-  xs.reduce((acc, ys) => acc.concat(ys), [])
-
-const processesList = Object.values(processes)
+import processes from '~/static/process.json'
 
 export default {
   middleware: 'is-reviewer',
 
   async asyncData({ app, params }) {
-    const {id} = params
+    const { id } = params
 
     const data = {
-      levels,
-      process,
-      isModalActive: false,
-      processAttributes: [],
       project: {},
       organization: {},
       unit: {},
-      evidences: [],
       evidencesDb: {},
       selectedLevels: [],
       selectedProcesses: [],
       projectEvidences: [],
-      newEvidence: Object.assign({}, emptyProjectEvidence),
-      dropFiles: [],
-      expectedResultsByProcess: app.mps.expectedResultsByProcess,
-      processAttributeBySelectedLevel: {}
+      expectedResultsByProcess: app.mps.expectedResultsByProcess
     }
 
     data.project = await app.$axios.$get(`/projects/${id}`)
     data.unit = await app.$axios.$get(`/units/${data.project.unitId}`)
     data.organization = await app.$axios.$get(`/organizations/${data.unit.organizationId}`)
-    data.evidences = await app.$axios.$get('/evidences')
-    data.evidencesDb = data.evidences
+
+    const evidences = await app.$axios.$get('/evidences')
+    data.evidencesDb = evidences
       .reduce((acc, evidence) => {
         acc[evidence.id] = evidence
         return acc
       }, {})
+
     data.roles = await app.$axios.$get('/roles')
     data.projectEvidences = await app.$axios.$get(`/projects/${id}/evidences`)
 
@@ -210,12 +187,6 @@ export default {
       .filter(({id}) => selectedProcessesSet.has(id))
     data.selectedAttributes = levels[data.unit.levelId].attributes
 
-    data.processAttributeBySelectedLevel = Object.entries(data.unit.selectedFeatures)
-      .reduce((acc, [level, { attributes }]) => {
-        acc[level] = attributes.sort()
-        return acc
-      }, {})
-
     return data
   },
 
@@ -226,56 +197,6 @@ export default {
   },
 
   computed: {
-    filteredRows() {
-      const validLevels = new Set(this.selectedLevels)
-      const validProcessAttributes = new Set(
-        levels[this.project.levelId].attributes
-      )
-
-      return this.projectEvidences
-        .map(projectEvidence => {
-          const { type, filename, evidenceId } = projectEvidence
-          const id = projectEvidence.typeId
-
-          const maybeProcess = type === 'expectedResult'
-            ? expectedResults[id] && process[expectedResults[id].process]
-            : false
-
-          const getLevelFromProcess = process =>
-            process.levels
-              ? process.levels[process.levels.length - 1]
-              : process.level
-
-          const level = type === 'expectedResult'
-            ? (maybeProcess ? getLevelFromProcess(maybeProcess) : '-')
-            : this.project.levelId
-
-          // Blank invalid
-
-          const getProcessAttributeId = attributeWithSubId =>
-            // Example 'AP 5.1 (IV)' => 'AP 5.1'
-            attributeWithSubId.split(' ').slice(0, -1).join(' ')
-
-          if (
-            (type === 'expectedResult' && !expectedResults[id]) ||
-            (type === 'processAttribute' && !processAttributes[getProcessAttributeId(id)]) ||
-            !this.evidencesDb[evidenceId]
-          ) return
-
-          const evidence = this.evidencesDb[evidenceId].name
-
-          return {
-            type,
-            id,
-            level,
-            filename,
-            evidence,
-            original: projectEvidence
-          }
-        })
-        .filter(projectEvidence => projectEvidence)
-    },
-
     expectedResultsByProcessWithEvidence () {
       return Object.entries(this.expectedResultsByProcess)
         .reduce((acc, [process, expectedResults]) => {
