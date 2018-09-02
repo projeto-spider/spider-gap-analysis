c0c8c8426759fd10d57e6fd7585bb36fe04d783e
lubien <lubien1996@gmail.com>
2018-09-02 17:08:39 -0300

    Create and use auth layout

---
+++ resources/layouts/auth.vue
@@ -0,0 +1,116 @@
+<template>
+  <section class="hero is-fullheight auth-hero">
+    <div class="hero-body is-paddingless" style="width: 100%;">
+      <div class="columns is-marginless" style="width: 100%; min-height: 100vh">
+        <div class="column column-form flex" style="flex-direction: column">
+          <div class="is-hidden-desktop has-text-centered">
+            <a href="https://github.com/projeto-spider/spider-gap-analysis" target="_blank">
+              <img class="logo gap-logo" :src="gapLogo">
+            </a>
+          </div>
+
+          <div class="login">
+            <nuxt/>
+          </div>
+        </div>
+
+        <div class="column is-hidden-touch column-wallpaper">
+          <div class="oblique"></div>
+          <div style="z-index: 2; text-align: center">
+            <a href="http://spider.ufpa.br/" target="_blank">
+              <img class="logo spider-logo" :src="spiderLogo">
+            </a>
+
+            <a href="https://portal.ufpa.br/" target="_blank">
+              <img class="logo ufpa-logo" :src="ufpaLogo">
+            </a>
+
+            <a href="https://github.com/projeto-spider/spider-gap-analysis" target="_blank">
+              <img class="logo gap-logo" :src="gapLogo">
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import gapLogo from '~/assets/spider-gap-logo-alt.png'
+import spiderLogo from '~/assets/spider-logo.png'
+import ufpaLogo from '~/assets/ufpa-logo.png'
+
+export default {
+  data: () => ({
+    gapLogo,
+    spiderLogo,
+    ufpaLogo
+  }),
+
+  computed: {
+    ...mapGetters(['loggedUser', 'isLoggedIn', 'isAdmin', 'isReviewer']),
+  },
+
+  methods: {
+    async logout() {
+      await this.$store.dispatch('logout')
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style lang="sass" src="@/assets/overrides.scss"></style>
+<style lang="sass" src="@/assets/auth-layout.sass" scoped></style>
+
+
+<style scoped>
+.auth-hero .column-form, .auth-hero .column-wallpaper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+/* Thanks: https://vuestic.epicmax.co/#/auth/login */
+.auth-hero .column-wallpaper {
+  background-color: #2c3e50;
+  overflow: hidden;
+  position: relative;
+}
+
+.auth-hero .column-wallpaper .oblique {
+  position: absolute;
+  background-color: #34495e;
+  left: 36.5%;
+  -webkit-transform: rotate(15deg);
+  transform: rotate(15deg);
+  width: 27%;
+  height: 500vh;
+}
+
+.auth-hero .column-form .login {
+  z-index: 2;
+}
+
+.logo {
+  height: auto;
+  margin-left: 32px;
+}
+
+.column-logos {
+  text-align: right;
+}
+
+.gap-logo {
+  max-width: 224px;
+}
+
+.spider-logo {
+  max-width: 128px;
+}
+
+.ufpa-logo {
+  width: 180px;
+}
+</style>
--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -1,28 +1,28 @@
 <template>
-  <section class="container">
-    <div class="logo-container">
-      <a href="https://github.com/projeto-spider/spider-gap-analysis" target="_blank">
-        <img :src="gapLogo" alt="SPIDER Gap Analysis">
-      </a>
-    </div>
+  <div class="login">
+    <form @submit.prevent="login">
+      <div class="field">
+        <label class="label has-text-centered is-size-4">Usuário</label>
+        <div class="control">
+          <input class="input is-large" type="text" v-model="username" autofocus>
+        </div>
+      </div>
 
-    <form action="fake" @submit.prevent="login">
-      <b-field label="Nome de usuário">
-        <b-input v-model="username"></b-input>
-      </b-field>
+      <div class="field">
+        <label class="label has-text-centered is-size-4">Senha</label>
+        <div class="control">
+          <input class="input is-large" type="password" v-model="password">
+        </div>
+      </div>
 
-      <b-field label="Senha">
-        <b-input
-          type="password"
-          v-model="password"
-          password-reveald
-          autocomplete="password"
-        ></b-input>
-      </b-field>
 
-      <button type="submit" class="button is-primary">Login</button>
+      <div class="field is-grouped is-grouped-centered">
+        <button class="button is-primary is-large is-fullwidth">
+          Entrar
+        </button>
+      </div>
     </form>
-  </section>
+  </div>
 </template>
 
 <script>
@@ -32,11 +32,17 @@ import gapLogo from '~/assets/spider-gap-logo-alt.png'
 export default {
   middleware: 'logged-off',
 
+  layout: 'auth',
+
   data: () => ({
     gapLogo,
     username: '',
     password: '',
   }),
+
+  mounted () {
+    this.username = localStorage.getItem('lastUsername') || ''
+  },
 
   computed: {
     ...mapGetters(['loggedUser', 'isLoggedIn']),
@@ -45,13 +51,21 @@ export default {
   methods: {
     async login() {
       const { username, password } = this
-      await this.$store.dispatch('login', { username, password })
-        .catch(this.$translateError('Falha no login'))
+
+      this.$store.dispatch('login', { username, password })
+        .then(this.success)
+        .catch(this.$translateError('Usuário ou senha incorretos.'))
+
+
+    },
+
+    success () {
+      localStorage.setItem('lastUsername', this.username)
 
       this.$success('Login com sucesso!')
 
       this.$router.push({ path: '/about', success: true})
-    },
+    }
   }
 }
 </script>
