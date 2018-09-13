8e1bd8b89110d5d447ed2001c89dfc8daaaed6a3
lubien <lubien1996@gmail.com>
2018-09-13 10:03:55 -0300

    save

--- resources/pages/about.vue
+++ resources/pages/about.vue
@@ -1,10 +1,23 @@
 <template>
-  <section class="container page-container">
-    <h1 class="title">
+  <section class="container" style="align-self: flex-start;">
+    <div class="columns">
+      <div class="column is-one-third" v-for="(metric, i) in metricList" :key="i">
+        <article class="message is-dark" :class="{[metric.color]: true}" style="box-shadow: 0 0 0.5rem rgba(83,78,76,.5);">
+          <div class="message-header"></div>
+
+          <div class="message-body has-text-centered">
+            <p class="is-size-2">{{ metric.count }}</p>
+            <p class="is-size-6">{{ metric.name }}</p>
+          </div>
+        </article>
+      </div>
+    </div>
+
+    <h1 class="title" v-if="false">
       Bem vindo(a) <strong>{{ loggedUser.username }}</strong>
     </h1>
 
-    <div class="logo-container">
+    <div class="logo-container" v-if="false">
       <a href="https://github.com/projeto-spider/spider-gap-analysis" target="_blank">
         <img :src="gapLogo" alt="Projeto SPIDER">
       </a>
@@ -21,10 +34,31 @@ export default {
 
   data: () => ({
     gapLogo,
+
+    metrics: {
+      'Organizações': 0,
+      'Unidades Organizacionais': 0,
+      'Projetos': 0
+    }
   }),
 
   computed: {
     ...mapGetters(['loggedUser']),
+
+    metricList () {
+      const colorFor = {
+        0: 'is-link',
+        1: 'is-dark',
+        2: 'is-info'
+      }
+
+      return Object.entries(this.metrics)
+        .map(([key, value], i) => ({
+          name: key,
+          count: value,
+          color: colorFor[i]
+        }))
+    }
   },
 
   head () {
