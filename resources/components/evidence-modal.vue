ccc85a75795eb3dc82c7480a50dd6d7e84b2d435
lubien <lubien1996@gmail.com>
2018-09-15 16:41:39 -0300

    Refactor Project Evidence page
    * Show project evidences in a modal component.
    * Collapse projects
    * Better indexing for project evidences

---
+++ resources/components/evidence-modal.vue
@@ -0,0 +1,65 @@
+<template>
+  <b-modal :active.sync="isActive" @close="onClose">
+    <div class="card" v-if="projectEvidence">
+      <div v-if="isImage" class="card-image">
+        <figure class="image">
+          <img :src="`/uploads/${projectEvidence.filename}`" alt="Image">
+        </figure>
+      </div>
+
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4">
+              <a :href="`/uploads/${projectEvidence.filename}`" target="_blank">
+                {{ projectEvidence.filename }}
+              </a>
+            </p>
+
+            <p class="title is-5">
+              {{ evidence.name }}
+            </p>
+
+            <p class="subtitle is-6">
+              <strong>Fonte de Evidência: </strong>{{ role.name }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </b-modal>
+</template>
+
+<script>
+import path from 'path'
+
+export default {
+  name: 'EvidenceModal',
+
+  props: {
+    isActive: Boolean,
+    projectEvidence: [Boolean, Object],
+    evidence: [Boolean, Object],
+    role: [Boolean, Object]
+  },
+
+  computed: {
+    isImage () {
+      if (!this.projectEvidence) {
+        return false
+      }
+
+      const filename = this.projectEvidence.filename
+      const extension = path.extname(filename)
+
+      return ['.png', '.jpeg', '.jpg', '.gif'].includes(extension)
+    }
+  },
+
+  methods: {
+    onClose () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
--- resources/pages/project-evidences/_id.vue
+++ resources/pages/project-evidences/_id.vue
@@ -5,81 +5,122 @@
 
     <p><h4 class="title is-4">Associar Projetos às Evidências</h4></p>
 
-    <div
+    <b-collapse
       v-for="process in selectedProcesses"
       :key="process.id"
+      :open="false"
+      class="card"
+      style="margin-bottom: 15px"
     >
-      <h5 class="title is-5">{{ process.id }}</h5>
+      <div slot="trigger" slot-scope="props" class="card-header">
+        <p class="card-header-title">
+          <span>{{ process.description }}</span>
+        </p>
 
-      <table class="table is-fullwidth">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>Ações</th>
-          </tr>
-        </thead>
+        <span v-if="progresses[process.id]" class="card-header-icon">
+          <progress
+            class="progress is-info"
+            style="width: 100px"
+            :value="progresses[process.id].filled"
+            :max="progresses[process.id].total"
+          ></progress>
+        </span>
+      </div>
 
-        <tbody>
-          <tr v-for="result in expectedResultsByProcess[process.id]" :key="result.id">
-            <td>{{result.id}}</td>
-            <td>
-              <template
-                v-if="projectEvidences.some(ev => ev.type === 'expectedResult' && ev.typeId == result.id)"
+      <div class="card-content" style="padding: 0">
+        <div class="content">
+          <table class="table is-fullwidth is-striped" style="border-top: 1px solid #dedede;">
+            <thead>
+              <tr>
+                <th style="min-width: 150px; text-align: center">ID</th>
+                <th>Descrição</th>
+                <th style="min-width: 300px; text-align: center">Ações</th>
+              </tr>
+            </thead>
+
+            <tbody>
+              <tr v-for="result in expectedResultsByProcess[process.id]" :key="result.id">
+                <td style="min-width: 150px; text-align: center">
+                  {{result.id}}
+                </td>
+
+                <td>
+                  <p>{{ result.description }}</p>
+                  <p class="has-text-grey" style="font-size: .9em">{{ result.evidence }}</p>
+                </td>
+
+                <td style="min-width: 300px; text-align: center">
+                  <template
+                    v-if="evidencesFor['expectedResult'][result.id]"
+                  >
+                    <button
+                      @click="openEvidence('expectedResult', result.id)"
+                      class="button is-primary"
+                    >Ver Evidência</button>
+                    <button
+                      @click="destroy('expectedResult', result.id)"
+                      class="button is-danger"
+                    >Excluir</button>
+                  </template>
+
+                  <button
+                    v-else
+                    @click="openModal('expectedResult', result.id)"
+                    class="button is-secondary"
+                  >Inserir Evidência</button>
+                </td>
+              </tr>
+
+              <tr>
+                <td colspan="3" style="background-color: rgb(121, 121, 121); color: #fff">
+                  Atributos de Processo
+                </td>
+              </tr>
+
+              <tr
+                v-for="attr in selectedAttributes"
+                :key="attr.id"
               >
-                <button
-                  @click="openEvidence('expectedResult', result.id)"
-                  class="button is-primary"
-                >Ver Evidência</button>
-                <button
-                  @click="destroy('expectedResult', result.id)"
-                  class="button is-danger"
-                >Excluir</button>
-              </template>
+                <td style="min-width: 150px; text-align: center">{{ attr.id }}</td>
 
-              <button
-                v-else
-                @click="openModal('expectedResult', result.id)"
-                class="button is-secondary"
-              >Inserir Evidência</button>
-            </td>
-          </tr>
+                <td>
+                  <p>{{ attr.description }}</p>
+                  <p class="has-text-grey" style="font-size: .9em">{{ attr.evidence }}</p>
+                </td>
 
-          <tr
-            v-for="attr in selectedAttributes"
-            :key="attr"
-          >
-            <td>{{ attr }}</td>
-            <td>
-              <template
-                v-if="projectEvidences.some(ev => ev.type === 'processAttribute' && ev.typeId == `${process.id}-${attr}`)"
-              >
-                <button
-                  @click="openEvidence('processAttribute', `${process.id}-${attr}`)"
-                  class="button is-primary"
-                >Ver Evidência</button>
-                <button
-                  @click="destroy('processAttribute', `${process.id}-${attr}`)"
-                  class="button is-danger"
-                >Excluir</button>
-              </template>
+                <td style="min-width: 300px; text-align: center">
+                  <template
+                    v-if="evidencesFor['processAttribute'][`${process.id}-${attr.id}`]"
+                  >
+                    <button
+                      @click="openEvidence('processAttribute', `${process.id}-${attr.id}`)"
+                      class="button is-primary"
+                    >Ver Evidência</button>
+                    <button
+                      @click="destroy('processAttribute', `${process.id}-${attr.id}`)"
+                      class="button is-danger"
+                    >Excluir</button>
+                  </template>
 
-              <button
-                v-else
-                @click="openModal('processAttribute', `${process.id}-${attr}`)"
-                class="button is-secondary"
-              >Inserir Evidência</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
+                  <button
+                    v-else
+                    @click="openModal('processAttribute', `${process.id}-${attr.id}`)"
+                    class="button is-secondary"
+                  >Inserir Evidência</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </b-collapse>
 
     <b-modal :active.sync="isModalActive" @close="dropFiles = []">
       <form @submit.prevent="create">
         <div class="modal-card">
           <section class="modal-card-body">
             <b-field label="Evidência">
-              <b-select v-model="newEvidence.evidenceId" placeholder="Selecione uma evidência">
+              <b-select v-model="newEvidence.evidenceId" placeholder="Selecione uma evidência" expanded required>
                 <option
                   v-for="option in evidences"
                   :value="option.id"
@@ -90,7 +131,7 @@
             </b-field>
 
             <b-field label="Fonte de Evidência">
-              <b-select v-model="newEvidence.roleId" placeholder="Selecione uma fonte de evidência">
+              <b-select v-model="newEvidence.roleId" placeholder="Selecione uma fonte de evidência" expanded required>
                 <option
                   v-for="option in roles"
                   :value="option.id"
@@ -100,29 +141,39 @@
               </b-select>
             </b-field>
 
+            <b-field label="Arquivo"></b-field>
+
             <b-field class="file">
-              <b-upload v-model="dropFiles">
+              <b-upload v-model="dropFiles" required>
                 <a class="button is-primary">
                   <b-icon icon="upload"></b-icon>
-                  <span>Selecionar arquivo</span>
+                  <span style="">Selecionar arquivo</span>
                 </a>
               </b-upload>
 
-              <span class="file-name" v-if="dropFiles && dropFiles.length">
+              <span class="file-name upload-expanded" v-if="dropFiles && dropFiles.length">
                 {{ dropFiles[0].name }}
               </span>
 
-              <span class="file-name" v-else>Nenhum arquivo selecionado</span>
+              <span class="file-name upload-expanded" v-else>Nenhum arquivo selecionado</span>
             </b-field>
           </section>
 
-          <footer class="modal-card-foot">
+          <footer class="modal-card-foot" style="justify-content: flex-end;">
             <button class="button" type="button" @click="closeModal()">Fechar</button>
             <button class="button is-primary" type="submit">Salvar</button>
           </footer>
         </div>
       </form>
     </b-modal>
+
+    <evidence-modal
+      :isActive="!!modalEvidence"
+      :projectEvidence="modalEvidence"
+      :evidence="modalEvidence.evidence"
+      :role="modalEvidence.role"
+      @close="modalEvidence = false"
+    ></evidence-modal>
   </section>
 </template>
 
@@ -131,6 +182,8 @@ import FormData from 'form-data'
 import { toRoman } from 'roman-numerals'
 import levels from '~/static/levels.json'
 import processes from '~/static/process.json'
+
+import EvidenceModal from '~/components/evidence-modal.vue'
 
 const emptyProjectEvidence = () => ({
   projectId: 0,
@@ -145,11 +198,17 @@ const emptyProjectEvidence = () => ({
 export default {
   middleware: 'is-admin',
 
+  components: { EvidenceModal },
+
   async asyncData({ app, params }) {
     const { id } = params
 
     const data = {
       isModalActive: false,
+      dropFiles: [],
+
+      modalEvidence: false,
+
       processAttributes: [],
       project: {},
       organization: {},
@@ -158,10 +217,10 @@ export default {
       selectedLevels: [],
       projectEvidences: [],
       newEvidence: emptyProjectEvidence(),
-      dropFiles: [],
       selectedProcesses: [],
       selectedAttributes: [],
       expectedResultsByProcess: app.mps.expectedResultsByProcess,
+      processAttributesByLevel: app.mps.processAttributesByLevel,
       processAttributeBySelectedLevel: {}
     }
 
@@ -173,7 +232,7 @@ export default {
     data.projectEvidences = await app.$axios.$get(`/projects/${id}/evidences`)
 
     data.selectedProcesses = app.mps.getUnitProcesses(data.unit)
-    data.selectedAttributes = levels[data.unit.levelId].attributes
+    data.selectedAttributes = app.mps.processAttributesByLevel[data.unit.levelId]
 
     return data
   },
@@ -181,6 +240,74 @@ export default {
   head () {
     return {
       title: 'Evidência e Projeto'
+    }
+  },
+
+  computed: {
+    progresses () {
+      return this.selectedProcesses
+        .reduce((acc, process) => {
+          const expectedResultIds = process.id === 'Nível A'
+            ? []
+            : this.expectedResultsByProcess[process.id].map(({ id }) => id)
+          const expectedResults = new Set(expectedResultIds)
+
+          const processAttributeIds = this.selectedAttributes.map(
+            ({ id }) => `${process.id}-${id}`
+          )
+          const processAttributes = new Set(processAttributeIds)
+
+          const total = expectedResults.size + processAttributes.size
+
+          const filled =
+            this.projectEvidences
+              .filter(ev => {
+                if (ev.type === 'expectedResult') {
+                  return expectedResults.has(ev.typeId)
+                }
+
+                if (ev.type === 'processAttribute') {
+                  return processAttributes.has(ev.typeId)
+                }
+              })
+              .length
+
+          acc[process.id] = {
+            total,
+            filled
+          }
+
+          return acc
+        }, {})
+    },
+
+    evidencesFor () {
+      const evidencesById = this.evidences
+        .reduce((acc, evidence) => {
+          acc[evidence.id] = evidence
+          return acc
+        }, {})
+
+      const roleById = this.roles
+        .reduce((acc, role) => {
+          acc[role.id] = role
+          return acc
+        }, {})
+
+      const initialAcc = {
+        expectedResult: {},
+        processAttribute: {}
+      }
+
+      return this.projectEvidences
+        .reduce((acc, projectEvidence) => {
+          acc[projectEvidence.type][projectEvidence.typeId] = {
+            ...projectEvidence,
+            evidence: evidencesById[projectEvidence.evidenceId],
+            role: roleById[projectEvidence.roleId]
+          }
+          return acc
+        }, initialAcc)
     }
   },
 
@@ -198,8 +325,8 @@ export default {
     },
 
     openEvidence(type, id) {
-      const evidence = this.projectEvidences.find(ev => ev.type == type && ev.typeId == id)
-      window.open(`/uploads/${evidence.filename}`, '_blank')
+      const evidence = this.evidencesFor[type][id]
+      this.modalEvidence = evidence
     },
 
     async create() {
@@ -222,16 +349,32 @@ export default {
     },
 
     async destroy(type, id) {
-      const evidence = this.projectEvidences.find(ev => ev.type == type && ev.typeId == id)
-      const {id: projectId} = this.$route.params
+      this.$dialog.confirm({
+        title: 'Deletando evidência do projeto',
+        message: 'Esta ação não pode ser desfeita',
+        cancelText: 'Cancelar',
+        confirmText: 'Deletar',
+        type: 'is-danger',
+        onConfirm: async () => {
+          const evidence = this.evidencesFor[type][id]
+          const {id: projectId} = this.$route.params
 
-      const data = await this.$axios.$delete(`/projects/${projectId}/evidences/${evidence.id}`)
-        .catch(this.$translateError('Falha ao deletar'))
+          const data = await this.$axios.$delete(`/projects/${projectId}/evidences/${evidence.id}`)
+            .catch(this.$translateError('Falha ao deletar'))
 
-      this.$success('Excluído com sucesso')
+          this.$success('Excluído com sucesso')
 
-      this.projectEvidences = this.projectEvidences.filter(ev => ev.id !== evidence.id)
+          this.projectEvidences = this.projectEvidences.filter(ev => ev.id !== evidence.id)
+        }
+      })
     }
   }
 }
 </script>
+
+<style scoped>
+.upload-expanded {
+  width: 100%;
+  max-width: 100%;
+}
+</style>
